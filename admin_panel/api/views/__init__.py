70755b29cde608ca2a25977313f71620e1cc9a62
shagorrobidas <roysagor88@gmail.com>
2026-04-06 04:55:52 +0600

    feat(auth): export VerifyEmailView

--- admin_panel/api/views/__init__.py
+++ admin_panel/api/views/__init__.py
@@ -1,6 +1,48 @@
-from .auth_views import RegisterView, LoginView, LogoutView, ProfileView, ForgotPasswordView, ResetPasswordView
+from .auth_views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    ProfileView,
+    ForgotPasswordView,
+    ResetPasswordView,
+    VerifyEmailView
+)
 from .dashboard_views import AdminDashboardView
-from .user_views import UserListView, UserCreateView, UserDetailView, ToggleUserActiveView
-from .task_views import AdminTaskListCreateView, AdminTaskDetailView
+from .user_views import (
+    UserListView,
+    UserCreateView,
+    UserDetailView,
+    ToggleUserActiveView
+)
+from .task_views import (
+    AdminTaskListCreateView,
+    AdminTaskDetailView
+)
 from .settings_views import BusinessSettingsView
-from .report_views import AdminReportView, TransactionListView, TransactionApproveView
+from .report_views import (
+    AdminReportView,
+    TransactionListView,
+    TransactionApproveView
+)
+
+
+__all__ = [
+    'RegisterView',
+    'LoginView',
+    'LogoutView',
+    'ProfileView',
+    'ForgotPasswordView',
+    'ResetPasswordView',
+    'AdminDashboardView',
+    'UserListView',
+    'UserCreateView',
+    'UserDetailView',
+    'ToggleUserActiveView',
+    'AdminTaskListCreateView',
+    'AdminTaskDetailView',
+    'BusinessSettingsView',
+    'AdminReportView',
+    'TransactionListView',
+    'TransactionApproveView',
+    'VerifyEmailView'
+]
