1d3793d7478e461ffa61d23b9b1fda9effb8e883
shagorrobidas <roysagor88@gmail.com>
2026-04-06 00:32:47 +0600

    feat(views): update frontend views for dashboard redirection

--- api/views/frontend_views.py
+++ api/views/frontend_views.py
@@ -2,20 +2,26 @@
 
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+
 
 class LoginView(TemplateView):
     template_name = 'login.html'
 
+
 class RegisterView(TemplateView):
     template_name = 'register.html'
+
 
 class ForgotPasswordView(TemplateView):
     template_name = 'forgot-password.html'
 
+
 class ProfileView(TemplateView):
     template_name = 'profile.html'
+
 
 class LogoutView(TemplateView):
     template_name = 'logout.html'
