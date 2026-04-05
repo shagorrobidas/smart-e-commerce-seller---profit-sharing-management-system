c0f9457a686a9749245682c0d788756a3836aaa3
shagorrobidas <roysagor88@gmail.com>
2026-04-06 00:32:47 +0600

    refactor(core): update project URL patterns

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Import patterns from admin_panel api file specifically for legacy support if needed
+# Import patterns from admin_panel api file specifically 
+# for legacy support if needed
 from admin_panel.api.urls import auth_urlpatterns
 
 urlpatterns = [
@@ -25,4 +27,8 @@ urlpatterns = [
     # Root Pages (Login, etc.)
     path('', include('api.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
